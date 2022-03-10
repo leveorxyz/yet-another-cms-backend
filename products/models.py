b3b33d4729701d066f3a1735c7093cfc1d1590b8
Sanviraj Zahin Haque <zahinjason220434@gmail.com>
2022-03-10 23:52:37 +0600

    add : basic category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.models import CoreModel
+from user.models import User
 
 # Create your models here.
+
+class Category(CoreModel):
+    title = models.CharField(max_length=100,default="",blank=False,null=False)
+    thumbnail = models.CharField(max_length=255, default=None, blank=True, null=True)
+    image_alt = models.CharField(max_length=255, default=None, null=True, blank=True)
+
+
+
+
