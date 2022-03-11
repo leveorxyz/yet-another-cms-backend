492a48e76f9f8c4d9e190a41df533c06ad6f451b
Sanviraj Zahin Haque <zahinjason220434@gmail.com>
2022-03-11 15:56:27 +0600

    add : product model

--- products/models.py
+++ products/models.py
@@ -9,6 +9,16 @@ class Category(CoreModel):
     thumbnail = models.CharField(max_length=255, default=None, blank=True, null=True)
     image_alt = models.CharField(max_length=255, default=None, null=True, blank=True)
 
-
-
+class Product(CoreModel):
+    title = models.CharField(max_length=100, default="", null=False, blank=False)
+    price = models.FloatField(default=0.0)
+    thumbnail = models.CharField(max_length=100, default=None, blank=True, null=True)
+    image_alt = models.CharField(max_length=100, default=None, null=True, blank=True)
+    is_trending = models.BooleanField(default=False, blank=True)
+    stock = models.PositiveIntegerField(default=0, blank=True)
+    tags = models.CharField(max_length=4096, default="", blank=True, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
+    )
+    description = models.CharField(max_length=500, null=True, blank=True, default=None)
 
