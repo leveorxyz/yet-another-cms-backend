2d821c894d574b32921a3f4d188b2de50e399309
Sanviraj Zahin Haque <zahinjason220434@gmail.com>
2022-03-12 18:26:03 +0600

    add : Tags model , Comments on all models for product app

--- products/models.py
+++ products/models.py
@@ -4,26 +4,33 @@ from user.models import User
 
 # Create your models here.
 
+#Category Class : Inherits from CoreModel 
 class Category(CoreModel):
-    title = models.CharField(max_length=100,default="",blank=False,null=False)
-    thumbnail = models.CharField(max_length=255, default=None, blank=True, null=True)
-    image_alt = models.CharField(max_length=255, default=None, null=True, blank=True)
+    title = models.CharField(max_length=100,default="",blank=False,null=False) #Title of the category
+    thumbnail = models.CharField(max_length=255, default=None, blank=True, null=True) #Thumbnail of the category
+    image_alt = models.CharField(max_length=255, default=None, null=True, blank=True) #Alternative text incase the thumbnail fails
 
+#Tag Class : Inherits from CoreModel
+class Tag(CoreModel):
+    title = models.CharField(max_length=255) #Title of the tag
+
+#Product Class : Inherits from CoreModel
 class Product(CoreModel):
-    title = models.CharField(max_length=100, default="", null=False, blank=False)
-    price = models.FloatField(default=0.0)
-    thumbnail = models.CharField(max_length=100, default=None, blank=True, null=True)
-    image_alt = models.CharField(max_length=100, default=None, null=True, blank=True)
-    is_trending = models.BooleanField(default=False, blank=True)
-    stock = models.PositiveIntegerField(default=0, blank=True)
-    tags = models.CharField(max_length=4096, default="", blank=True, null=True)
+    title = models.CharField(max_length=100, default="", null=False, blank=False) #Title of the product
+    price = models.FloatField(default=0.0) #Price of the product
+    thumbnail = models.CharField(max_length=100, default=None, blank=True, null=True) #Thumbnail of the product
+    image_alt = models.CharField(max_length=100, default=None, null=True, blank=True) #Alternative text incase the thumbnail fails
+    is_trending = models.BooleanField(default=False, blank=True) #Is the product trending
+    stock = models.PositiveIntegerField(default=0, blank=True) #Quantity of the product in stock
+    tag = models.ManyToManyField(Tag,on_delete = models.SET_NULL, blank=True,null=True,related_name="tags") #related tags of each product
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
-    )
-    description = models.CharField(max_length=500, null=True, blank=True, default=None)
+    ) #related category of each product
+    description = models.CharField(max_length=500, null=True, blank=True, default=None) #Description of the product
 
+#Product Image Class : Inherits from CoreModel
 class ProductPhoto(CoreModel):
     product = models.ForeignKey(
         Product, related_name="images", on_delete=models.CASCADE
-    )
-    url = models.CharField(max_length=100, default=None, blank=True, null=True)
+    ) #related product of each product image (Foreign Key)
+    url = models.CharField(max_length=255, default=None, blank=True, null=True) #URL of the product image
