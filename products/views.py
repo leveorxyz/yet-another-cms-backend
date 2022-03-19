21bf2a67d36b705abe6838e410c61e94a18143b0
Sanviraj Zahin Haque <zahinjason220434@gmail.com>
2022-03-19 15:04:27 +0600

    wip: product list view

--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import ProductFilter
+from core.permissions import ModeratorOrHigher
 from core.views import CustomListAPIView,CustomRetrieveUpdateDestroyAPIView,CustomCreateAPIView
 from .models import Category, Tag, Product
 from .serializers import CategorySerializer, TagSerializer, ProductSerializer
-from rest_framework.generics import CreateUpdateRetrieveDestroyAPIView
 
 # Create your views here.
 
@@ -20,4 +23,14 @@ class CategoryRetrieveUpdateDestroyView(CustomRetrieveUpdateDestroyAPIView):
 
 class ProductListView(CustomListAPIView):
     serializer_class = ProductSerializer
-    
+    filter_backends = [DjangoFilterBackend]
+    filter_class = [ProductFilter]
+    permission_classes = [
+        AllowAny,
+    ]
+    search_fields = ["title"]
+
+    def get_queryset(self):
+        base_query = Product.objects.filter(stock__gt=0)
+
+        return base_query
